061e99ec64fbe3f0cd6a658216023a4bebe57f58
Bill Proudfoot <billproudfoot@Bills-MacBook-Pro.local>
2017-08-29 10:07:26 +0100

    Initial Commit

---
+++ Average CalculatorTests/Average_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Average_CalculatorTests.swift
+//  Average CalculatorTests
+//
+//  Created by Bill Proudfoot on 29/08/2017.
+//  Copyright © 2017 Bill Proudfoot. All rights reserved.
+//
+
+import XCTest
+@testable import Average_Calculator
+
+class Average_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
